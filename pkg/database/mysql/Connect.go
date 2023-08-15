57909d2116e569fcca111a83ea3d6914772af1ca
yasin <yascanary0@gmail.com>
2023-08-15 14:58:46 +0330

    singleton mysql connection

---
+++ pkg/database/mysql/Connect.go
@@ -0,0 +1,25 @@
+package mysql
+
+import (
+	"sync"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var doOnce sync.Once
+var singletonConnection *gorm.DB
+
+func Connect() *gorm.DB {
+	doOnce.Do(func() {
+		dsn := "root:@tcp(127.0.0.1:3306)/go_elastic?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+
+        singletonConnection = db
+	})
+
+    return singletonConnection
+}
