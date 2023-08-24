ff5570073872ba5676f46ffab74252deb18cdc99
yasin <yascanary0@gmail.com>
2023-08-24 19:09:17 +0330

    disable default transactions in gorm

--- pkg/database/mysql/Connect.go
+++ pkg/database/mysql/Connect.go
@@ -13,7 +13,9 @@ var singletonConnection *gorm.DB
 func Connect() *gorm.DB {
 	doOnce.Do(func() {
 		dsn := "root:@tcp(127.0.0.1:3306)/go_elastic?charset=utf8mb4&parseTime=True&loc=Local"
-		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			SkipDefaultTransaction: true,
+		})
 		if err != nil {
 			panic(err)
 		}
