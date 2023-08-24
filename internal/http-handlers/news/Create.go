358f8937273c39759be7b328a89b27b29ad5b882
yasin <yascanary0@gmail.com>
2023-08-24 19:21:04 +0330

    index new records after creation

--- internal/http-handlers/news/Create.go
+++ internal/http-handlers/news/Create.go
@@ -2,11 +2,15 @@ package news
 
 import (
 	"elastic-search/internal/models/news"
+	"elastic-search/internal/services/elasticsearch"
+	"elastic-search/pkg/database/mysql"
 	"fmt"
 	"net/http"
 
 	newsValidation "elastic-search/internal/validations/news"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Create(c *gin.Context) {
@@ -20,7 +24,24 @@ func Create(c *gin.Context) {
 		Title:   params.Title,
 		Content: params.Content,
 	}
-	news.Save()
+
+	db := mysql.Connect()
+	err := db.Transaction(func(tx *gorm.DB) error {
+		if err := news.Save(); err != nil {
+			return err
+		}
+
+		if _, err := elasticsearch.Index("go_news_index", news); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "news created.", "news": news})
 }
