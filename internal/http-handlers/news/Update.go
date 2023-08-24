80228740d45cc6c29d50ef033f3e935cf10398c0
yasin <yascanary0@gmail.com>
2023-08-24 19:33:01 +0330

    update elastic index after update record in db

--- internal/http-handlers/news/Update.go
+++ internal/http-handlers/news/Update.go
@@ -2,11 +2,15 @@ package news
 
 import (
 	"elastic-search/internal/models/news"
+	"elastic-search/internal/services/elasticsearch"
+	"elastic-search/pkg/database/mysql"
 	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	newsValidation "elastic-search/internal/validations/news"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Update(c *gin.Context) {
@@ -22,9 +26,22 @@ func Update(c *gin.Context) {
 		return
 	}
 
-	err := newsItem.Update(news.News{
-		Title:   params.Title,
-		Content: params.Content,
+	db := mysql.Connect()
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := newsItem.Update(news.News{
+			Title:   params.Title,
+			Content: params.Content,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if _, err := elasticsearch.Index("go_news_index", newsItem); err != nil {
+			return err
+		}
+
+		return nil
 	})
 
 	if err != nil {
